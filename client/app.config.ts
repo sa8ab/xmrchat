dfb7cb11a3a437a0b9ce500322037cbe7b5b42fe
sa8ab <bwsaeed8@gmail.com>
2024-08-18 19:21:53 +0330

    sync

--- client/app.config.ts
+++ client/app.config.ts
@@ -2,6 +2,7 @@ export default defineAppConfig({
   ui: {},
   socials: {
     twitter: "xmr_chat",
-    email: "support@xmrchat.org",
+    email: "support@xmrchat.com",
   },
+  minXMRPayAmount: 0.001,
 });
