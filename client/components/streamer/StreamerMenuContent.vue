67d8b020104a778f60368f0ebf517685325f42c0
sa8ab <bwsaeed8@gmail.com>
2024-11-25 20:37:54 +0330

    init streamer menu lyout

---
+++ client/components/streamer/StreamerMenuContent.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: StreamerMenu
+  </div>
+</template>
+
+<style scoped></style>
---
+++ client/layouts/streamer.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout name="default">
+    <div class="grid grid-cols-12 inner">
+      <div class="col-span-3">
+        <StreamerMenu />
+      </div>
+      <div class="col-span-9">
+        <slot />
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="scss"></style>
