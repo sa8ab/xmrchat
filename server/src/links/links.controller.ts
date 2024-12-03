275a5e7d3ed1167e2d7eed4a201bece806b3e2e5
sa8ab <bwsaeed8@gmail.com>
2024-12-03 19:03:07 +0330

    add entities

--- server/src/links/links.controller.ts
+++ server/src/links/links.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Put,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PagesService } from 'src/pages/pages.service';
+import { CurrentUser } from 'src/shared/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { LinksService } from './links.service';
+import { UpdateLinksDto } from './dto/update-links.dto';
 
 @Controller('links')
-export class LinksController {}
+export class LinksController {
+  constructor(
+    private linksService: LinksService,
+    private pagesService: PagesService,
+  ) {}
+
+  @Get('/me')
+  async getMyLinks(@CurrentUser() user: User) {
+    const page = await this.pagesService.findMyPage(user);
+
+    if (!page) throw new NotFoundException('Page is not found.');
+
+    if (page.userId !== user.id) throw new UnauthorizedException();
+
+    return this.linksService.findByPageId(page.id);
+  }
+
+  @Put('/me')
+  async updateMyContentLinks(
+    @CurrentUser() user: User,
+    @Body() body: UpdateLinksDto,
+  ) {}
+}
--- server/src/links/links.service.ts
+++ server/src/links/links.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Link } from './link.entity';
+import { Repository } from 'typeorm';
+import { PagesService } from 'src/pages/pages.service';
 
 @Injectable()
-export class LinksService {}
+export class LinksService {
+  constructor(
+    private pagesService: PagesService,
+    @InjectRepository(Link) private repo: Repository<Link>,
+  ) {}
+
+  async findByPageId(id: number) {
+    if (!id) return null;
+
+    return this.repo.findBy({ page: { id } });
+  }
+
+  async updateContentLinks() {}
+}
