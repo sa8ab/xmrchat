a996286de12b2e1eb330eb6dbbf69558cdef8038
David Havlicek <snex00@protonmail.com>
2024-12-08 11:12:11 -0800

    cap password length at 72 characters

--- client/pages/auth/signup.vue
+++ client/pages/auth/signup.vue
@@ -5,7 +5,7 @@ useHead({
   title: "Sign up",
 });
 
-const { required, email, sameAs, validate } = useValidations();
+const { required, email, maxLength, sameAs, validate } = useValidations();
 const { signup } = useAuthStore();
 const { toLogin } = useRouteLocation();
 const toast = useToast();
@@ -22,7 +22,7 @@ const state = reactive({
 const v = useVuelidate(
   computed(() => ({
     email: { required, email },
-    password: { required },
+    password: { required, maxLength: maxLength(72) },
     confirmPassword: { sameAs: sameAs(state.password, "password") },
   })),
   state
