f44f4481052f161fbcee404fdd1968eefc6eab90
sa8ab <bwsaeed8@gmail.com>
2024-12-24 18:01:54 +0330

    get coins on startapp

--- client/plugins/startApp.ts
+++ client/plugins/startApp.ts
@@ -1,8 +1,10 @@
 export default defineNuxtPlugin(async () => {
   const price = useState("xmrPrice");
+  const coins = useState("coins");
 
   await useAsyncData(async () => {
     price.value = await $fetch("/get-price");
+    coins.value = await $fetch("/coins");
     return {};
   });
 });
