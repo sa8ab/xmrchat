dda87b02ae372e41263ffc432cf4e056ded4c558
sa8ab <bwsaeed8@gmail.com>
2024-12-24 23:06:51 +0330

    public get coins request

--- server/src/integrations/trocador/trocador.controller.ts
+++ server/src/integrations/trocador/trocador.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { TrocadorService } from './trocador.service';
+import { IsPublic } from 'src/shared/decorators/is-public.decorator';
 
 @Controller('trocador')
 export class TrocadorController {
   constructor(private trocadorService: TrocadorService) {}
 
+  @IsPublic()
   @Get('/get-coins')
   getCoins() {
     return this.trocadorService.getAndSaveCoins();
