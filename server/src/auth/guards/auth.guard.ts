6197f20bad23421031833d7e5d9c980731cc763b
sa8ab <bwsaeed8@gmail.com>
2025-02-25 03:53:52 +0330

    Fix includes

--- server/src/auth/guards/auth.guard.ts
+++ server/src/auth/guards/auth.guard.ts
@@ -35,6 +35,6 @@ export class AuthGuard implements CanActivate {
     if (!requiredRoles) return true;
 
     // user exists and some roles exist too.
-    return requiredRoles.some((rr: string) => user.roles?.inclues(rr));
+    return requiredRoles.some((rr: string) => user.roles?.includes(rr));
   }
 }
