50ad5882443606b1692b4f821b18d31280e5c7a9
sa8ab <bwsaeed8@gmail.com>
2025-02-26 13:57:04 +0330

    Init admin layout

---
+++ client/layouts/admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout name="default">
+    <div class="flex-1 lg:grid lg:grid-cols-12 lg:gap-6 inner">
+      <div class="lg:col-span-3">
+        <StreamerMenu />
+      </div>
+      <div class="lg:col-span-9 py-4">
+        <slot />
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="scss"></style>
