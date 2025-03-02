f60d0119e83bfa8f7986a14dccdc0900e9e4e033
sa8ab <bwsaeed8@gmail.com>
2025-03-02 20:32:15 +0330

    Add deactivating page

--- server/src/admin/admin-pages.controller.ts
+++ server/src/admin/admin-pages.controller.ts
@@ -1,15 +1,19 @@
 import {
+  Body,
   Controller,
   Get,
   NotFoundException,
   Param,
+  ParseEnumPipe,
+  Put,
   Query,
 } from '@nestjs/common';
-import { RolesEnum } from 'src/shared/constants';
+import { PageStatusEnum, RolesEnum } from 'src/shared/constants';
 import { Roles } from 'src/shared/decorators/roles.decorator';
 import { Serialize } from 'src/shared/interceptors/serialize.interceptor';
 import { AdminPageRO, PageSearchAdminRO } from './dtos/admin-page.dto';
 import { PagesService } from 'src/pages/pages.service';
+import { ChangePageStatusDto } from './dtos/change-page-status.dto';
 
 @Roles(RolesEnum.ADMIN)
 @Controller('/admin/pages')
@@ -31,5 +35,15 @@ export class AdminPagesController {
     if (!path) throw new NotFoundException();
     const page = await this.pagesServices.findAdminPageByPath(path);
     return { page };
+  }
+
+  @Put('/:path/change-status')
+  async changeStatus(
+    @Param('path') path: string,
+    @Body() body: ChangePageStatusDto,
+  ) {
+    const status = body.status;
+    await this.pagesServices.changeStatus(path, status);
+    return { message: 'Status changed.' };
   }
 }
