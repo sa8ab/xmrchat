246d204375770274ea0c9c7fc018ce404dbfdac4
Ehsan <xdehsanxd@gmail.com>
2025-03-13 15:43:48 +0330

    Add French and Spanish language support to i18n configuration and locale files

--- client/locales/fr.ts
+++ client/locales/fr.ts
@@ -11,4 +11,116 @@ export default {
     url: "The link is invalid.",
     notUrl: "Should not be a link.",
   },
+
+  account: "Account",
+  logout: "Logout",
+  login: "Login",
+  email: "Email",
+  search: "Search",
+  password: "Password",
+  confirmPassword: "Confirm Password",
+  signupInstead: "Signup Instead",
+  loginInstead: "Login Instead",
+  signup: "Signup",
+  forgetPassword: "Forget Password ?",
+  loginDescription:
+    "Login to your account in order to access your display page.",
+  creatorLogin: "Creator Login",
+  searchCreators: {
+    title: "Search Creators",
+    description: "Search creators with public pages on xmrchat.",
+  },
+  contactUs: "Contact Us",
+
+  xmrchat: "XMRChat",
+  heroDescription: "Tip your favorite streamer in cryptocurrency.",
+  findCreators: "Find Creators",
+  creatorsStartHere: "Creators Start Here",
+
+  FAQ: {
+    title: "FAQ",
+    whyTipWithXMRChat: "Why tip with XMRChat?",
+    whyUseMonero: "Why does XMRChat use Monero?",
+    whereToGetMonero: "Where to get Monero?",
+  },
+
+  earnMore: {
+    title: "Earn more",
+    description:
+      "YouTube charges 30-50% for Superchats. Rumble 20% for Rants. With XMRChat you receive 100% of your XMR tips from fans, directly to your Monero wallet.",
+  },
+  censorshipResistant: {
+    title: "Censorship Resistant",
+    description:
+      "Accepting Monero tips allows you to have an income that is not directly connected to big tech and arbitrary policy violations.",
+  },
+  corruptionResistant: {
+    title: "Corruption Resistant",
+    description:
+      "Many regimes seize or freeze bank accounts of political enemies. Monero allows you to be your own bank with complete control of your finances.",
+  },
+  private: {
+    title: "Private",
+    description:
+      "Viewers can tip anonymously, no worries from employers or others knowing your donations.",
+  },
+  quickAndEasy: {
+    title: "Quick And Easy",
+    description:
+      "No need for viewers to create an account, verify their email, or enter credit card info. Fans go to your tip page, type their name, message, amount, and submit their XMR tip.",
+  },
+  openSource: {
+    title: "Open Source",
+    description: "XMRChat is open source. You can review the code",
+  },
+
+  lowFees: {
+    title: "Low fees",
+    description: "Monero transaction fees are a few pennies at most.",
+  },
+  privateMonero: {
+    title: "Private",
+    description:
+      "While most crypto uses open ledgers, Monero hides transaction data. Sender, receiver, and transaction amounts are not revealed by looking at the blockchain.",
+  },
+  accepted: {
+    title: "Accepted",
+    description:
+      "Avoid the hassle of selling your crypto on a centralized exchange. Buy goods and services directly from merchants that accept Monero on",
+  },
+  andMore: {
+    title: "And more",
+    description: "Learn more about Monero at",
+  },
+
+  // TIP PAGE
+  tipName: "Name",
+  tipAmount: "Amount",
+  tipMessage: "Message",
+  tipPrivate: "Private",
+  tipPrivateTooltip: "Name and message will be only visible to the streamer.",
+  tipCoin: "Coin",
+  tipCoinPlaceholder: "XMR",
+  sendTip: "Send Tip",
+  tipCreationFailed: "Tip Creation Failed",
+  tipNamePlaceholder: "Enter name",
+  tipAmountPlaceholder: "Enter amount",
+  tipMessagePlaceholder: "Enter message",
+  tipPrivatePlaceholder: "Private",
+  tipSwapUnavailable: "Swap is currently unavailable.",
+  tipSwapMinimum: "Minimum {minSwapUSD}$ to swap",
+  minUsdAmount: "Minimum {minUsdAmount}",
+  recentTips: "Recent Tipsss",
+  tipDisplayValueTooltip: "Show tip values in XMR or USD",
+  noRecentTips: "No recent tips!",
+
+  tipWalletWarningTitle: "Do not tip with streamer wallet.",
+  tipWalletWarningDescription:
+    "Please avoid sending tips with wallet registered on the page. The change returned inflates the amount we see received.",
+  tipWalletMinimum:
+    "Please send a minimum {minimumAmount} XMR to the following address for your xmrchat to be displayed.",
+  openInMyWallet: "Open in My Wallet",
+  cancel: "Cancel",
+  copyAddress: "Copy Address",
+  waitingForPayment: "Waiting for payment",
 };
