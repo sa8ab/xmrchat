be3c6b5e727f2faada1041a79494824614b9dfb9
sa8ab <bwsaeed8@gmail.com>
2025-05-16 15:10:16 +0330

    Cleanup

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,17 +1,9 @@
 <script lang="ts" setup>
 useHead({});
-const { $axios } = useNuxtApp();
-const sendRequest = async () => {
-  const res = await $axios.post("/i18n-test", {
-    count: 1,
-  });
-  console.log(res.data);
-};
 </script>
 
 <template>
   <div class="inner">
-    <UButton @click="sendRequest">Test</UButton>
     <HomeHero />
     <HomeFAQ />
   </div>
