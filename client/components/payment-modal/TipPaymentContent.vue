80c0b4dc87ac653280aff26d2ada71a1460e3b09
sa8ab <bwsaeed8@gmail.com>
2025-06-12 17:19:46 +0330

    Add tip payment modal

---
+++ client/components/payment-modal/TipPaymentContent.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import type { TipCreationResponse } from "~/types";
+
+const props = defineProps<{
+  createdTip?: TipCreationResponse;
+  connectionStatus?: string;
+}>();
+
+const emit = defineEmits<{
+  cancel: [];
+  retry: [];
+}>();
+</script>
+
+<template></template>
