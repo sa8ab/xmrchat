6c461331194d2ef83efd343dce9a1e3e038e655a
sa8ab <bwsaeed8@gmail.com>
2025-07-05 13:36:30 +0330

    Add saving notification preferences

--- client/pages/streamer/notification-preferences/index.vue
+++ client/pages/streamer/notification-preferences/index.vue
@@ -6,6 +6,7 @@ import {
 } from "~/types/enums";
 
 const { axios } = useApp();
+const toast = useToast();
 
 type Form = {
   [key in NotificationChannelEnum]: {
@@ -14,12 +15,14 @@ type Form = {
 };
 interface State {
   form: Form;
+  pending: boolean;
 }
 
 const state = reactive<State>({
   form: {
     [NotificationChannelEnum.EMAIL]: {},
   },
+  pending: false,
 });
 
 const getObjectPereferences = (
@@ -47,6 +50,38 @@ await useLazyAsyncData(
   },
   { server: false }
 );
+
+const handleSave = async () => {
+  const convertedPreferences = Object.entries(state.form)
+    .map(([channel, p]) => {
+      return Object.entries(p).map(([type, enabled]) => {
+        return {
+          channel,
+          type,
+          enabled,
+        };
+      });
+    })
+    .flatMap((p) => p);
+
+  state.pending = true;
+  try {
+    await axios.patch("/notification-preferences", {
+      preferences: convertedPreferences,
+    });
+    toast.add({
+      color: "green",
+      title: "Notification preferences saved.",
+    });
+  } catch (error) {
+    toast.add({
+      color: "red",
+      title: getErrorMessage(error),
+    });
+  } finally {
+    state.pending = false;
+  }
+};
 </script>
 
 <template>
@@ -55,13 +90,20 @@ await useLazyAsyncData(
       title="Notification Preferences"
       description="Manage your notification preferences"
     />
-    <div class="grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-4">
-      <NotificationPreferenceContainer
-        :channel="NotificationChannelEnum.EMAIL"
-        v-model="state.form.email"
-      >
-      </NotificationPreferenceContainer>
-    </div>
+    <GeneralForm @submit="handleSave">
+      <div class="grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-4">
+        <NotificationPreferenceContainer
+          :channel="NotificationChannelEnum.EMAIL"
+          v-model="state.form.email"
+        >
+        </NotificationPreferenceContainer>
+      </div>
+      <div class="flex mt-4">
+        <UButton type="submit" color="primary" :loading="state.pending">
+          Save
+        </UButton>
+      </div>
+    </GeneralForm>
   </div>
 </template>
 
