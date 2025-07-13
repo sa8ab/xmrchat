183825703ba09a9146bba987a4a4663e8160a730
sa8ab <bwsaeed8@gmail.com>
2025-07-14 03:05:09 +0330

    Init integrations page

---
+++ client/pages/streamer/integrations/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import useVuelidate from "@vuelidate/core";
+import type { NotificationPreference, Numberic } from "~/types";
+import {
+  NotificationChannelEnum,
+  NotificationPreferenceType,
+} from "~/types/enums";
+
+const { axios } = useApp();
+const toast = useToast();
+const { numberic } = useValidations();
+const authStore = useAuthStore();
+const { dayjs } = useDate();
+
+interface State {
+  // form: Form;
+  pending: boolean;
+}
+
+const state = reactive<State>({
+  pending: false,
+});
+
+const v = useVuelidate<any>({}, state);
+
+const { getValidationAttrs } = useValidations(v);
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Notifications" description="Manage your notifications" />
+
+    <!-- <ErrorView :error="error" v-if="error" /> -->
+  </div>
+</template>
+
+<style scoped></style>
