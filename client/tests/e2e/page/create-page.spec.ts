a3f3eb694c9f38d1a8c75dda796dc11a97b055a3
sa8ab <bwsaeed8@gmail.com>
2025-07-24 21:39:11 +0330

    Add creating page test

--- client/tests/e2e/page/create-page.spec.ts
+++ client/tests/e2e/page/create-page.spec.ts
@@ -1,3 +1,25 @@
 import { test } from "@playwright/test";
+import path from "path";
+import { fileURLToPath } from "url";
 
-test("Create page", async ({ page }) => {});
+const __filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(__filename);
+
+test("Create page", async ({ page }) => {
+  await page.goto("/streamer/create", { waitUntil: "networkidle" });
+
+  // first file inpput
+  const logoInput = page.getByLabel("Click to upload").first();
+
+  const fileChooserPromise = page.waitForEvent("filechooser");
+  await logoInput.click();
+  const fileChooser = await fileChooserPromise;
+  await fileChooser.setFiles(path.join(dirname, "xmrchat-logo.png"));
+
+  const bannerInput = page.getByLabel("Click to upload").nth(1);
+  await bannerInput.click();
+  const fileChooser2 = await fileChooserPromise;
+  await fileChooser2.setFiles(path.join(dirname, "xmrchat-logo.png"));
+
+  await page.waitForTimeout(4000);
+});
