ef6653175e15dce60ca7f97dbe6a248696aadaef
sa8ab <bwsaeed8@gmail.com>
2025-07-26 03:03:32 +0330

    Add Playwright service to docker-compose and update configuration for test environment

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig<ConfigOptions>({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3002",
+    baseURL: process.env.TEST_BASE_URL || "http://localhost:3002",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
