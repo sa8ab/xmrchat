569f7d6fba73a4107991af21586774f0640a11fb
sa8ab <bwsaeed8@gmail.com>
2025-08-17 20:35:59 +0330

    Update

--- client/pages/streamer/recipients/index.vue
+++ client/pages/streamer/recipients/index.vue
@@ -1,4 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { type PageRecipient } from "@/types/general";
+import { PageRecipientVariant } from "~/types/enums";
+
+const state = reactive<{
+  recipients: PageRecipient[];
+}>({
+  recipients: [],
+});
+
+const { axios } = useApp();
+const {} = useLazyAsyncData(
+  async () => {
+    // const {data} = await axios.get('/page-recipients')
+  },
+  { server: false }
+);
+
+const pageRecipient = computed(() => {
+  return (
+    state.recipients.find(
+      ({ variant }) => variant === PageRecipientVariant.PAGE
+    ) || {
+      variant: PageRecipientVariant.PAGE,
+      name: "Page Address",
+    }
+  );
+});
+
+const xmrChatRecipient = computed(() => {
+  return (
+    state.recipients.find(
+      ({ variant }) => variant === PageRecipientVariant.XMRCHAT
+    ) || {
+      variant: PageRecipientVariant.XMRCHAT,
+      address: "xmrchat address",
+      name: "XMRChat",
+    }
+  );
+});
+</script>
 
 <template>
   <div>
@@ -22,7 +62,7 @@
         <RecipientItem editableAddress editablePercentage showDelete />
       </div>
       <div class="flex gap-2 mt-4">
-        <UButton>Save</UButton>
+        <UButton type="">Save</UButton>
         <UButton variant="outline">Add recipient</UButton>
       </div>
     </GeneralForm>
