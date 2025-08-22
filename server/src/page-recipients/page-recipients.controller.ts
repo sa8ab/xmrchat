9361251cc788a34f7f597da6255e142fd63f2311
sa8ab <bwsaeed8@gmail.com>
2025-08-22 15:03:31 +0330

    Add casl for updating recipients

--- server/src/page-recipients/page-recipients.controller.ts
+++ server/src/page-recipients/page-recipients.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PageRecipientsService } from './page-recipients.service';
 import { UpdateRecipientsDto } from './dtos/update-recipient.dto';
 import { CurrentUser } from 'src/shared/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
+import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
+import { Action } from 'src/shared/constants';
 
 @Controller('page-recipients')
 export class PageRecipientsController {
-  constructor(private pageRecipientsService: PageRecipientsService) {}
+  constructor(
+    private pageRecipientsService: PageRecipientsService,
+    private casl: CaslAbilityFactory,
+  ) {}
 
   @Get('/')
   async findMyRecipients(@CurrentUser() user: User) {
@@ -22,7 +33,12 @@ export class PageRecipientsController {
     @Body() dto: UpdateRecipientsDto,
     @CurrentUser() user: User,
   ) {
+    const ability = this.casl.createForUser(user);
+    if (!ability.can(Action.Update, 'recipient')) {
+      throw new UnauthorizedException('Unauthorized to change recipients');
+    }
+
     await this.pageRecipientsService.updateRecipients(dto, user);
-    return { message: 'Updated recipients' };
+    return { message: 'Recipients updated.' };
   }
 }
