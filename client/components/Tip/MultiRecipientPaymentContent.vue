c0acfa61258038f4c525016672a8a176e5f9117e
sa8ab <bwsaeed8@gmail.com>
2025-08-30 16:07:40 +0330

    Update list of recipients in tip page and change titles

--- client/components/Tip/MultiRecipientPaymentContent.vue
+++ client/components/Tip/MultiRecipientPaymentContent.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import type { TipCreationResponse } from "~/types";
+import type { PageRecipient, TipCreationResponse } from "~/types";
+import { PageRecipientVariant } from "~/types/enums";
 
 const props = defineProps<{
   createdTip?: TipCreationResponse;
@@ -15,6 +16,19 @@ const emit = defineEmits<{
 }>();
 
 const { expired, remaining, initialize } = usePaymentExpiration();
+
+const sortedRecipients = computed(() => {
+  return props.createdTip?.tipRecipients.sort(
+    (a, b) => Number(b.percentage) - Number(a.percentage)
+  );
+});
+
+const getRecipientName = (recipient: PageRecipient) => {
+  if (recipient.variant === PageRecipientVariant.PAGE)
+    return `${props.slug} address`;
+  if (recipient.variant === PageRecipientVariant.XMRCHAT) return `XMRChat`;
+  return recipient.name;
+};
 
 // Using watch cause we are not sure if when component mounts the props are passed to it.
 watch(
@@ -49,14 +63,14 @@ watch(
           {{ $t("recipients") }}
         </h3>
         <div
-          v-for="(recipient, index) in createdTip?.tipRecipients"
+          v-for="(recipient, index) in sortedRecipients"
           :key="index"
           class="p-3 border border-border rounded-lg"
         >
           <div class="flex justify-between items-start mb-2">
             <div>
               <h4 class="font-medium">
-                {{ recipient.name || `Recipient ${index + 1}` }}
+                {{ getRecipientName(recipient) }}
               </h4>
               <p class="text-sm text-muted-foreground">
                 {{ recipient.percentage }}%
