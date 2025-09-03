c8ab47c7d01ab9e798bf1a052d899568a7f02bc5
sa8ab <59901734+sa8ab@users.noreply.github.com>
2025-09-03 20:47:40 +0330

    Use logger for test

--- server/src/test/test.controller.ts
+++ server/src/test/test.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Logger, Post } from '@nestjs/common';
 import { TestService } from './test.service';
 import { IsPublic } from 'src/shared/decorators/is-public.decorator';
 
 @Controller('__test__')
 export class TestController {
+  private logger = new Logger(TestController.name);
+
   constructor(private readonly testService: TestService) {}
 
   @Post('seed')
   @IsPublic()
   async seed() {
-    console.log('Running seed');
+    this.logger.log('Running seed');
 
     await this.testService.init();
   }
