bdfd30a2fa5c5b7941c089f914da63c89f6e8c44
imxmr <imxmr@posteo.de>
2025-09-11 03:46:16 -0500

    revert: useLinkify composables reverted

--- client/composables/useLinkify.ts
+++ client/composables/useLinkify.ts
@@ -1,15 +1,20 @@
 import linkifyStr from 'linkify-string';
 import DOMPurify from "isomorphic-dompurify";
 
-export const linkifyAndSanitize = (message: string | null) => {
-  if (!message) return;
-  const options = { 
-      defaultProtocol: 'https',
-      target: { url: '_blank'},
-      attributes: { rel: 'nofollow noopener noreferrer'},
-      className: "text-primary hover:text-primary-400 hover:underline hover:underline-offset-4 hover:decoration-primary-400"
-   };
-  const linkifiedMessage = linkifyStr(message, options);
-  const sanitizedMessage = DOMPurify.sanitize(linkifiedMessage);
-  return sanitizedMessage;
+export const useLinkify = () => {
+    const linkifyAndSanitize = (message: string | null) => {
+        if (!message) return;
+        const options = { 
+            defaultProtocol: 'https',
+            target: { url: '_blank'},
+            attributes: { rel: 'nofollow noopener noreferrer'},
+            className: "text-primary hover:text-primary-400 hover:underline hover:underline-offset-4 hover:decoration-primary-400"
+         };
+        const linkifiedMessage = linkifyStr(message, options);
+        const sanitizedMessage = DOMPurify.sanitize(linkifiedMessage);
+        return sanitizedMessage;
+    }
+    return {
+        linkifyAndSanitize
+    }
 }
