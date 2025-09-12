caed8b5c969a67dd2a36feaf53d9aeac3f478ac0
imxmr <imxmr@posteo.de>
2025-09-12 18:42:33 -0500

    feat: useMarkdown and useSanitize updated

--- client/composables/useSanitize.ts
+++ client/composables/useSanitize.ts
@@ -1,7 +1,18 @@
 import DOMPurify from "isomorphic-dompurify";
-
-export const useSanitize = (text: string | null) => {
+const ALLOWED_TAGS = ["a", "strong", "em"];
+const ALLOWED_ATTR = ["href", "target", "rel", "class"];
+export const useSanitize = (
+    text: string,
+    isAllowListEnabled: boolean
+) => {
     if (!text) return;
+    if (isAllowListEnabled) {
+        const sanitizedText = DOMPurify.sanitize(text, {
+            ALLOWED_TAGS: ALLOWED_TAGS,
+            ALLOWED_ATTR: ALLOWED_ATTR
+        });
+        return sanitizedText;
+    }
     const sanitizedText = DOMPurify.sanitize(text);
     return sanitizedText;
 }
