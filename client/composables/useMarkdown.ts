a53b1f79c478d90a7cc5d3e4886dcebd9b846790
imxmr <imxmr@posteo.de>
2025-09-14 08:09:43 -0500

    fix: sanitize composable import updated

--- client/composables/useMarkdown.ts
+++ client/composables/useMarkdown.ts
@@ -1,6 +1,7 @@
 import markdownit from "markdown-it";
 import mila from "markdown-it-link-attributes";
 export const useMarkdown = () => {
+  const { sanitize } = useSanitize();
   const attributes = {
     target: "_blank",
     rel: "nofollow noopener noreferrer",
@@ -18,7 +19,6 @@ export const useMarkdown = () => {
 
   const markdownAndSanitize = (message: string | null) => {
     if (!message) return;
-    const { sanitize } = useSanitize();
     const sanitizedMessageWithoutMarkdown = sanitize(message, {
       isAllowListEnabled: false,
     });
