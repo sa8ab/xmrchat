9df1199dcb2b1d91332c2853b11242c1200bd678
imxmr <imxmr@posteo.de>
2025-09-14 07:57:12 -0500

    chore: formatting, changing import settings

--- client/composables/useMarkdown.ts
+++ client/composables/useMarkdown.ts
@@ -1,34 +1,39 @@
 import markdownit from "markdown-it";
 import mila from "markdown-it-link-attributes";
-const { sanitize } = useSanitize();
 export const useMarkdown = () => {
-    const attributes = { 
-        target: "_blank",
-        rel: "nofollow noopener noreferrer",
-        class: "text-primary hover:text-primary-400 hover:underline hover:underline-offset-4 hover:decoration-primary-400",
-    };
+  const attributes = {
+    target: "_blank",
+    rel: "nofollow noopener noreferrer",
+    class:
+      "text-primary hover:text-primary-400 hover:underline hover:underline-offset-4 hover:decoration-primary-400",
+  };
 
-    const md = markdownit("zero", {
-        linkify: true,
-        typographer: true,
-    }).enable(["linkify", "emphasis"]);
+  const md = markdownit("zero", {
+    linkify: true,
+    typographer: true,
+  }).enable(["linkify", "emphasis"]);
 
-    md.linkify.tlds('onion', true).add("ftp:", null);
-    md.use(mila, { attrs: attributes });
+  md.linkify.tlds("onion", true).add("ftp:", null);
+  md.use(mila, { attrs: attributes });
 
-    const markdownAndSanitize = (message: string | null) => {
-        if (!message) return;
-        const sanitizedMessageWithoutMarkdown = sanitize(message, { isAllowListEnabled: false });
-        if (!sanitizedMessageWithoutMarkdown) return;
+  const markdownAndSanitize = (message: string | null) => {
+    if (!message) return;
+    const { sanitize } = useSanitize();
+    const sanitizedMessageWithoutMarkdown = sanitize(message, {
+      isAllowListEnabled: false,
+    });
+    if (!sanitizedMessageWithoutMarkdown) return;
 
-        const markdownMessage = md.renderInline(sanitizedMessageWithoutMarkdown);
-        const sanitizedMarkdownMessage = sanitize(markdownMessage, { isAllowListEnabled: true });
-        if (!sanitizedMarkdownMessage) return;
+    const markdownMessage = md.renderInline(sanitizedMessageWithoutMarkdown);
+    const sanitizedMarkdownMessage = sanitize(markdownMessage, {
+      isAllowListEnabled: true,
+    });
+    if (!sanitizedMarkdownMessage) return;
 
-        return sanitizedMarkdownMessage;
-    }
-    
-    return {
-        markdownAndSanitize
-    }
-}
+    return sanitizedMarkdownMessage;
+  };
+
+  return {
+    markdownAndSanitize,
+  };
+};
--- client/composables/useSanitize.ts
+++ client/composables/useSanitize.ts
@@ -1,24 +1,20 @@
 import DOMPurify from "isomorphic-dompurify";
 
 export const useSanitize = () => {
-    const sanitize = (
-        text: string,
-        options: { isAllowListEnabled: boolean }
-    ) => {
-        const ALLOWED_TAGS = ["a", "strong", "em"];
-        const ALLOWED_ATTR = ["href", "target", "rel", "class"];
-        if (!text) return;
-        if (options.isAllowListEnabled) {
-            const sanitizedText = DOMPurify.sanitize(text, {
-            ALLOWED_TAGS: ALLOWED_TAGS,
-            ALLOWED_ATTR: ALLOWED_ATTR
-        });
-        return sanitizedText;
-    }
-        const sanitizedText = DOMPurify.sanitize(text);
-        return sanitizedText;    
-    }
-    return {
-        sanitize
-    }
-}
+  const sanitize = (text: string, options: { isAllowListEnabled: boolean }) => {
+    const ALLOWED_TAGS = ["a", "strong", "em"];
+    const ALLOWED_ATTR = ["href", "target", "rel", "class"];
+    const sanitizeOptions = options.isAllowListEnabled
+      ? {
+          ALLOWED_TAGS: ALLOWED_TAGS,
+          ALLOWED_ATTR: ALLOWED_ATTR,
+        }
+      : {};
+    if (!text) return;
+    const sanitizedText = DOMPurify.sanitize(text, sanitizeOptions);
+    return sanitizedText;
+  };
+  return {
+    sanitize,
+  };
+};
