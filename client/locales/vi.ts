5e37620132f2abd9749149b0f9d1f94441066a70
imxmr <imxmr@posteo.de>
2025-09-18 22:43:28 -0500

    feat: vietnamese locales added

---
+++ client/locales/vi.ts
@@ -0,0 +1,400 @@
+export default {
+  validations: {
+    required: "Vùng này là bắt buộc",
+    email: "Email không hợp lệ",
+    numberic: "Cần điền số",
+    minLength: "Gồm ít nhất {length} ký tự",
+    maxLength: "Không vượt quá {length} ký tự",
+    minValue: "Gồm ít nhất {value}.",
+    sameAs: "Phải giống với {otherName}",
+    someFieldsAreIncorrect: "Một số vùng điền chưa chính xác",
+    url: "Đường liên kết không hợp lệ",
+    notUrl: "Không điền đường liên kết",
+    streamerSlug:
+      "Đường dẫn (path) của bạn chỉ được phép chứa các ký tự chữ thường, chữ số, dấu gạch dưới, và dấu gạch ngang",
+    moneroPrimaryAddress: "Địa chỉ chính không hợp lệ. Nên bắt đầu với 4.",
+  },
+
+  account: "Tài khoản",
+  logout: "Đăng xuất",
+  login: "Đăng nhập",
+  send: "Gửi",
+  email: "Email",
+  search: "Tìm kiếm",
+  password: "Mật khẩu",
+  confirm: "Xác nhận",
+  reconnect: "Kết nối lại",
+  confirmPassword: "Xác nhận mật khẩu",
+  signupInstead: "Thay vào đó, hãy đăng ký",
+  signupFailed: "Đăng ký không thành công",
+  noItems: "Không có mục nào",
+  signupSuccessfull: "Đăng ký thành công",
+  signupSuccessfullDescription:
+    "Vui lòng nhấp vào liên kết được gửi tới email của bạn để xác minh tài khoản.",
+  loginInstead: "Thay vào đó, hãy đăng nhập",
+  loginFailed: "Đăng nhập không thành công",
+  signup: "Đăng ký",
+  forgetPassword: "Quên mật khẩu?",
+  loginDescription:
+    "Đăng nhập vào tài khoản của bạn để truy cập trang hiển thị.",
+  resetPassword: "Đặt lại mật khẩu",
+  enterYourNewPassword: "Nhập mật khẩu mới",
+  passwordResetFailed: "Đặt lại mật khẩu không thành công",
+  passwordUpdated: "Mật khẩu của bạn được cập nhật thành công",
+  weSentYouAnEmail:
+    "Chúng tôi đã gửi liên kết đến {email}, vui lòng nhấp vào liên kết trong email để đặt lại mật khẩu.",
+  changeEmail: "Thay đổi email",
+  enterEmailToResetPassword: "Nhập email của bạn để đặt lại mật khẩu.",
+  creatorLogin: "Đăng nhập dành cho nhà sáng tạo",
+  goToHomePage: "Về Trang Chủ",
+  searchCreators: {
+    title: "Tìm nhà sáng tạo",
+    description: "Tìm nhà sáng tạo có trang công khai trên xmrchat.",
+  },
+  typePageName: "Nhập tên trang...",
+  contactUs: "Liên hệ",
+  here: "Tại đây",
+  backToHome: "Quay lại trang chủ",
+  somethingWentWrong: "Đã có lỗi xảy ra!",
+
+  xmrchat: "XMRChat",
+  heroDescription: "Ủng hộ streamer yêu thích của bạn bằng tiền điện tử.",
+  findCreators: "Tìm nhà sáng tạo",
+  creatorsStartHere: "Nhà sáng tạo bắt đầu từ đây",
+
+  FAQ: {
+    title: "Câu hỏi thường gặp",
+    whyTipWithXMRChat: "Tại sao nên gửi tiền ủng hộ bằng XMRChat?",
+    howToUseXMRChat: "Cách sử dụng XMRChat?",
+    whyUseMonero: "Vì sao XMRChat dùng Monero?",
+    whereToGetMonero: "Làm sao để có Monero?",
+  },
+
+  earnMore: {
+    title: "Kiếm nhiều hơn",
+    description:
+      "YouTube thu 30–50% phí từ Superchat. Rumble thu 20% từ Rants. Với XMRChat, bạn nhận toàn bộ 100% tip XMR từ fan, chuyển trực tiếp vào ví Monero của bạn.",
+  },
+  censorshipResistant: {
+    title: "Chống kiểm duyệt",
+    description:
+      "Nhận tip bằng Monero giúp bạn có thu nhập không bị ràng buộc bởi các tập đoàn công nghệ lớn hay những chính sách tùy tiện.",
+  },
+  corruptionResistant: {
+    title: "Chống tham nhũng",
+    description:
+      "Monero giúp bạn tự làm chủ tài chính, không lo tài khoản bị đóng băng hay tịch thu",
+  },
+  private: {
+    title: "Riêng tư",
+    description:
+      "Người xem có thể tip ẩn danh, bạn không cần lo ngại nhà tuyển dụng hay người khác biết về các khoản ủng hộ.",
+  },
+  quickAndEasy: {
+    title: "Nhanh chóng và dễ dàng",
+    description:
+      "Người xem không cần tạo tài khoản, xác minh email hay nhập thông tin thẻ. Fan chỉ cần vào trang tip của bạn, nhập tên, lời nhắn, số tiền và gửi tip XMR.",
+  },
+  openSource: {
+    title: "Mã nguồn mở",
+    description:
+      "XMRChat là mã nguồn mở. Bạn có thể xem xét mã nguồn tại {github}",
+  },
+
+  lowFees: {
+    title: "Phí thấp",
+    description: "Phí giao dịch Monero chỉ tốn vài xu là cùng",
+  },
+  privateMonero: {
+    title: "Riêng tư",
+    description:
+      "Khác với hầu hết tiền điện tử có sổ cái công khai, Monero ẩn thông tin giao dịch. Người gửi, người nhận và số tiền đều được giữ bí mật trên blockchain",
+  },
+  accepted: {
+    title: "Được chấp nhận",
+    description:
+      "Mua hàng hóa và dịch vụ trực tiếp bằng Monero tại {xmrbazaar} và {monerica}, không cần qua sàn giao dịch tập trung.",
+  },
+  andMore: {
+    title: "Và nhiều hơn nữa",
+    description:
+      "Tìm hiểu thêm về Monero tại {getmonero} và xem Monero được sử dụng ở đâu tại {moneroeco}.",
+  },
+  forContentCreators: {
+    title: "Dành cho nhà sáng tạo nội dung",
+    description: {
+      main: `Nhấp vào nút “Nhà sáng tạo bắt đầu từ đây". Tạo tài khoản XMRChat. Tạo trang Tip. Chia sẻ đường dẫn trang tip của bạn cho người xem.`,
+      tutorialLink:
+        "Alex Anarcho có hướng dẫn chi tiết về cách dùng XMRChat trên {youtube}.",
+      ifStreamyard: "Nếu bạn dùng Streamyard:",
+      ifOBS: "Nếu bạn dùng OBS:",
+      streamyard: `Vào menu Edit tip page và thêm tên kênh Twitch của bạn. Khi livestream bằng Streamyard, chọn Twitch làm điểm phát. Bot XMRChat (xmr-chat trên Twitch) sẽ tự động gửi tin nhắn tip và bạn có thể hiển thị ngay trên màn hình Streamyard. Logo bot XMRChat sẽ đẹp nhất khi dùng chế độ Minimal.`,
+      obs: `Vào mục "OBS". Bấm "Copy OBS Page Link". Thêm một Browser Source trong OBS rồi dán link vừa sao chép vào`,
+    },
+  },
+  forFans: {
+    title: "Dành cho người hâm mộ",
+    description:
+      "Truy cập đường dẫn tip page của nhà sáng tạo nội dung mà bạn muốn ủng hộ. Nhập tên người dùng và lời nhắn, chọn số tiền tip rồi gửi. Một hộp thoại sẽ hiện chi tiết giao dịch. Tin nhắn tip của bạn sẽ được hiển thị trên trang của streamer sau khi gửi thành công.",
+  },
+  whereToGetMonero: {
+    cakeWallet: "Mua hoặc đổi crypto khác lấy Monero trên {cakeWallet}.",
+    stealthex:
+      "Đổi crypto khác cho Monero (XMR) tại {stealthex} và một số nền tảng khác tại {kycnot}",
+    haveno: "Mua Monero mà không cần KYC tại {haveno}.",
+    kraken: "Mua Monero với KYC tại {kraken}.",
+    xmrbazaar: "Bán hàng hóa hoặc dịch vụ cho Monero tại {xmrbazaar}.",
+    gupax: "Đào Monero bằng ứng dụng {gupax}.",
+    kunoAnneMedia: "Tạo chiến dịch gây quỹ bằng Monero tại {kunoAnneMedia}.",
+    monerica:
+      "Accept Monero at your business and get listed at the {monerica} and {monerodirectory} pages and the business listings map on {xmrbazaar} and {bankexit}.",
+  },
+
+  // TIP PAGE
+  tipName: "Tên",
+  tipAmount: "Số tiền",
+  tipMessage: "Lời nhắn",
+  tipPrivate: "Riêng tư",
+  tipDate: "Ngày",
+  tipUpdated: "Đã cập nhật tip!",
+  tipPrivateTooltip: "Tên và lời nhắn chỉ hiển thị cho streamer.",
+  tipCoin: "Loại coin",
+  tipCoinPlaceholder: "XMR",
+  sendTip: "Gửi tip",
+  tipCreationFailed: "Tạo tip thất bại",
+  tipNamePlaceholder: "Nhập tên",
+  tipAmountPlaceholder: "Nhập số tiền",
+  tipMessagePlaceholder: "Nhập lời nhắn",
+  tipPrivatePlaceholder: "Riêng tư",
+  tipSwapUnavailable: "Hiện không thể đổi coin.",
+  tipSwapMinimum: "Tối thiểu {min} để đổi",
+  tipMinimum: "Tối thiểu {min}",
+  recentTips: "Tip gần đây",
+  tipDisplayValueTooltip: "Hiển thị giá trị tip bằng XMR hoặc {fiat}",
+  noRecentTips: "Chưa có tip nào gần đây!",
+  pageDeactivatedAlert:
+    "Trang của bạn đã bị vô hiệu hóa và sẽ không được hiển thị công khai. Vui lòng liên hệ hỗ trợ để biết thêm chi tiết.",
+  tipPrivateMessage: "Tin nhắn riêng tư",
+  tipWalletWarningTitle: "Không dùng ví của streamer để gửi tip.",
+  tipWalletWarningDescription:
+    "Vui lòng tránh gửi tip bằng ví được đăng ký trên trang. Tiền thừa trả về có thể khiến hệ thống hiển thị sai số tiền nhận được.",
+  tipWalletMinimum:
+    "Vui lòng gửi tối thiểu {minimumAmount} XMR tới địa chỉ sau để tip của bạn hiển thị trên xmrchat.",
+  partialAmountReceived:
+    "Đã nhận {partialAmount}, vui lòng gửi thêm {remainingAmount} còn lại.",
+  openInMyWallet: "Mở trong Ví của tôi",
+  cancel: "Hủy",
+  copyAddress: "Sao chép địa chỉ",
+  waitingForPayment: "Đang chờ thanh toán",
+
+  // CONTACT US PAGE
+  contactUsDescription: "Liên hệ với chúng tôi nếu bạn có câu hỏi hoặc góp ý.",
+  contactUsForm: "Biểu mẫu liên hệ",
+  contactUsFormDescription: "Điền biểu mẫu nếu bạn có câu hỏi hoặc góp ý.",
+  contactUsFormButton: "Liên hệ với chúng tôi",
+  emailUsDirectly: "Gửi email trực tiếp cho chúng tôi",
+  followUsOnTwitter: "Theo dõi chúng tôi trên Twitter",
+  twitter: "Twitter",
+
+  // CREATE PAGE
+  createPage: "Tạo trang",
+  getStartedCreatingPage: "Bắt đầu bằng cách tạo trang streamer của bạn",
+  home: "Trang chủ",
+  logo: "Logo",
+  logoRatio: "Tỉ lệ 1:1",
+  bannerImage: "Ảnh bìa",
+  bannerImageBestRatio: "Nên tải ảnh theo tỉ lệ 3:1",
+  yourId: "ID của bạn",
+  pageSlug: "Đường dẫn trang",
+  moneroPrmReciveAddress: "Địa chỉ nhận Monero chính",
+  prmMoneroReciveAdressBegin: "Địa chỉ nhận Monero chính bắt đầu bằng số 4.",
+  moneroSecretViewKey: "Khóa bí mật để xem giao dịch Monero",
+  weNeedSecretViewKey:
+    "Chúng tôi cần khóa này để xem các giao dịch gửi đến từ người xem. {whereToFind}",
+  whereToFindViewKey: "Tìm khóa này ở đâu?",
+  twitchChannelName: "Tên kênh Twitch",
+  nameOfYourTwitchChannel:
+    "Tên của kênh Twitch của bạn. Dùng để hiển thị tip trên Stream qua bot xmr_chat trên Twitch.",
+  optional: "Không bắt buộc",
+  minTipAmount: "Số tiền tip tối thiểu (XMR)",
+  defaultTipAmount: "Giá trị tip mặc định",
+  thisIsOnlyForDisplaying:
+    "Chỉ để hiển thị giá trị tip. Người xem có thể thay đổi khi gửi tip.",
+  publicPage: "Trang công khai (sẽ hiển thị trên trang tìm kiếm nhà sáng tạo).",
+  continue: "Tiếp tục",
+  clickToUpload: "Bấm để tải lên",
+  pageCreatedSuccessfully: "Trang đã được tạo thành công!",
+  slugReservedUntil: "Slug được tạm giữ đến ",
+  note: "Ghi chú",
+  pageUpdated: "Trang đã được cập nhật!",
+  loading: "Đang tải",
+  available: "Có sẵn",
+  unavailable: "Không khả dụng",
+  fiatUnit: "Đơn vị tiền pháp định",
+  fiatUnitHelp:
+    "Đơn vị dùng để hiển thị tiền tip khi chọn chế độ tiền pháp định.",
+  tipExpiration: "Thời hạn tip",
+  tipExpirationHelp: "Tip sẽ tự động biến mất sau khoảng thời gian đã chọn.",
+  tipExpirationWarning:
+    "Thay đổi thời hạn sẽ xóa tất cả xmrchats cũ hơn thời gian đã chọn.",
+  disappearsX: "Sẽ biến mất sau {time}",
+  never: "Không bao giờ",
+  xDays: "{count} ngày",
+  xMonths: "{count} tháng",
+  paymentIsExpired:
+    "Thanh toán đã hết hạn. Nếu bạn đã gửi rồi, vui lòng liên hệ hỗ trợ.",
+  pageReservationExpired: "Thời hạn giữ chỗ trang đã hết",
+  paymentCheckingDis: "Kiểm tra thanh toán bị ngắt kết nối.",
+  ifPaymentMade:
+    "Nếu đã thanh toán, giao dịch sẽ hiển thị trong danh sách tip.",
+  splitPaymentToMultipleRecipients: "Chia tip cho nhiều người",
+  backToSinglePayment: "Quay lại gửi một người nhận",
+  recipients: "Người nhận",
+
+  // STREAMER MENU
+  menu: "Menu",
+  myXmrchats: "Xmrchats của tôi",
+  editTipPage: "Chỉnh sửa trang tip",
+  tipPage: "Trang tip",
+  contentLinks: "Liên kết nội dung",
+  obs: "OBS",
+  show: "Hiện",
+  hide: "Ẩn",
+  youDontHavePage: "Bạn chưa có trang nào",
+  getStartedByCreatingPage: "Hãy bắt đầu bằng cách tạo một trang mới cho bạn",
+  createNewWallet:
+    "Hãy tạo một ví Monero mới để dùng với XMRChat. Chúng tôi chỉ cần xem các giao dịch liên quan đến tin nhắn XMRChat, ngoài ra không cần gì khác.",
+  createNewPage: "Tạo Trang Mới",
+
+  // ACCOUNT PAGE
+  changePassword: "Đổi mật khẩu",
+  updateYourLoginPassword: "Cập nhật mật khẩu đăng nhập",
+  currentPassword: "Mật khẩu hiện tại",
+  newPassword: "Mật khẩu mới",
+  repeatNewPassword: "Nhập lại mật khẩu mới",
+  contactSupportForEmailChange: "Liên hệ hỗ trợ nếu muốn đổi email.",
+
+  // EDIT PAGE
+  editPage: "Chỉnh sửa Trang",
+  customizeYourTipPage: "Tùy chỉnh trang tip của bạn.",
+  errorCreatingUpdatingPage: "Lỗi khi tạo/cập nhật trang",
+  tipAmountSuggestions: "Gợi ý số tiền tip",
+  pageWillBeAvailableAt: "Trang của bạn sẽ hiển thị tại {url}",
+  noSuggestedAmountsAdded:
+    "Chưa có mức gợi ý nào, bấm nút bên dưới để thêm mức mới.",
+  addTier: "Thêm mức",
+  remove: "Xóa",
+  name: "Tên",
+  amountUSD: "Số tiền (USD)",
+  amountFiat: "Số tiền ({fiat})",
+
+  // CONTENT LINKS
+  brandName: "Tên thương hiệu",
+  brandNameHelp: "Tên thương hiệu/nội dung. Có thể khác với slug.",
+  searchTerms: "Từ khóa tìm kiếm",
+  searchTermsHelp:
+    "Tìm kiếm nhà sáng tạo sẽ dựa trên slug trang, tên, và các từ khóa trong danh sách này.",
+  contentLinksDescription: "Tên, Từ khóa tìm kiếm và Liên kết nội dung",
+  contentLinksSecondDescription:
+    "Liên kết đến social hoặc website của bạn. Để bỏ một giá trị thì để trống ô.",
+  saveChanges: "Lưu thay đổi",
+  notUrlWithMessage: "Chỉ nhập tên, không nhập đường dẫn đầy đủ.",
+  changesAreSaved: "Thay đổi đã được lưu.",
+  errorSavingChanges: "Lỗi khi lưu thay đổi.",
+  xUsername: "Tên người dùng {platform}",
+  websiteLink: "Đường dẫn website",
+  youtubeChannel: "Kênh Youtube",
+  peertubeChannel: "Kênh Peertube",
+  podcastRssLink: "Liên kết RSS Podcast",
+  nostrPubKey: "Khóa công khai Nostr",
+  kunoFundraiser: "Chiến dịch gây quỹ Kuno",
+
+  // OBS
+  obsDescription: "Tiện ích và cài đặt cho OBS",
+  settingsAreUpdated: "Cài đặt đã được cập nhật.",
+  errorUpdatingSettings: "Lỗi khi cập nhật cài đặt",
+  toUseXMRchatsOnOBS:
+    "Để dùng XMRChat trên OBS, hãy copy đường dẫn trang OBS rồi thêm vào mục 'Browser' trong OBS Sources.",
+  copyOBSLink: "Copy đường dẫn trang OBS",
+  obsPageSettings: "Cài đặt trang OBS",
+  preventMessagesFromFading: "Giữ tin nhắn không biến mất",
+  preventMessagesFromFadingDescription:
+    "Khi bật, tip mới nhất sẽ được giữ lại trên màn hình. Nếu tắt, mỗi tin nhắn chỉ hiển thị trong 60 giây.",
+  playSound: "Phát âm thanh",
+  playSoundDescription: "Phát âm thanh trên trang OBS khi nhận tip mới.",
+  playSoundDescriptionLocal:
+    "Nếu bạn thử tính năng này trực tiếp trên trình duyệt, sau khi mở tab OBS hãy nhấp vào chỗ bất kỳ trên trang. Nếu không, trình duyệt sẽ chặn âm thanh vì chưa có tương tác với tab.",
+  autoShowTips: "Tự động hiện tip",
+  autoShowTipsDescription:
+    "Khi bật, tip sẽ tự động hiện trên trang OBS trong 60 giây.",
+
+  //  INTEGRATIONS PAGE
+  integrations: "Các tích hợp",
+  integrationsTitle: "Các tích hợp",
+  integrationsDes: "Quản lý các tích hợp của bạn",
+  notConnected: "Chưa kết nối.",
+  waitingForVerification: "Đang chờ xác minh.",
+  connected: "Đã kết nối",
+  connect: "Kết nối",
+  disconnect: "Ngắt kết nối",
+  simplexDescription:
+    "Ứng dụng nhắn tin đầu tiên không cần đăng ký tài khoản/ID.",
+  simplexIntegration: "Tích hợp Simplex",
+  enterYourSimplexLink:
+    "Nhập đường dẫn kết nối SimpleX rồi bấm Kết nối. Tài khoản XMRChat sẽ gửi yêu cầu kết nối đến SimpleX của bạn. Sau khi chấp nhận, bạn sẽ nhận được một mã. Nhập mã đó để hoàn tất kết nối.",
+  simplexIsConnectedTo: 'SimpleX đã kết nối với tài khoản "{name}".',
+  acceptRequestInSimplex:
+    "Hãy chấp nhận yêu cầu kết nối trong ứng dụng SimpleX và nhập mã bạn nhận được.",
+  confirmCode: "Xác nhận mã",
+  success: "Thành cônh",
+  openSimplexAndAccept:
+    "Mở ứng dụng SimpleX trên thiết bị của bạn và chấp nhận kết nối từ tài khoản XMRChat.",
+  error: "Lỗi",
+  simplexIsConnected: "Đã kết nối SimpleX.",
+  simplexDisconnected: "Đã ngắt kết nối SimpleX.",
+  simplexLink: "Đường dẫn SimpleX",
+  signalDescription:
+    "Trải nghiệm nhắn tin khác biệt, đặt quyền riêng tư lên hàng đầu, đồng thời vẫn đầy đủ các tính năng bạn mong đợi.",
+  signalIsConnected: "Đã kết nối Signal.",
+  signalIsDisconnected: "Đã ngắt kết nối Signal.",
+  signalIntegration: "Kết nối Signal",
+  signalIsConnectedTo: "Signal đã kết nối với tài khoản {number}.",
+  enterCodeinSignalApp: "Nhập mã được gửi vào ứng dụng Signal của bạn.",
+  enterSignalNumOrId:
+    "Nhập số điện thoại hoặc ID Signal của bạn. Chúng tôi sẽ gửi mã đến số điện thoại này để xác minh.",
+  signalPhoneNumOrId: "Số điện thoại hoặc ID Signal",
+  sendCode: "Gửi mã",
+  code: "Mã",
+
+  // NOTIFICATIONS-PREFERENCES PAGE
+  notifications: "Thông báo",
+  manageYourNotifs: "Quản lý thông báo của bạn",
+  comingSoon: "Sắp ra mắt",
+  featureAvailableSoon: "Tính năng này sẽ có trong thời gian tới",
+  minNotifsThreshold: "Ngưỡng thông báo tối thiểu",
+  minNotifsThresholdHelp:
+    "Số XMR tip tối thiểu cần đạt để kích hoạt thông báo.",
+  connectInIntegrations: "Kết nối trong {integrations} để bật tính năng.",
+  dailySummaryTimeHelp: "Thời gian gửi thông báo tóm tắt hằng ngày.",
+  notifsPreferencesSaved: "Đã lưu cài đặt thông báo.",
+  newTip: "Tip mới",
+  receiveNotifsWhenTipReceived: "Nhận thông báo khi có tip mới",
+  dailySummary: "Tóm tắt hằng ngày",
+  receiveDailySummary: "Nhận bản tóm tắt tip hằng ngày",
+  receiveNotifsViaEmail: "Nhận thông báo qua email",
+  receiveNotifsViaSimplex: "Nhận thông báo qua SimpleX",
+  receiveNotifsViaSignal: "Nhận thông báo qua Signal",
+
+  // HEAD
+  head: {
+    description:
+      "Người xem có thể gửi tin nhắn và tip dễ dàng, riêng tư. Streamer giữ gần như toàn bộ tip thay vì mất 30-50% cho các nền tảng lớn.",
+    title: "Nhắn tin và gửi tip bằng Monero",
+    XMRChatTip: "XMRChat - Gửi tip {path} bằng Monero",
+    tip: "Gửi tip {path}",
+  },
+  moneroSuperchats: "Monero Superchats : XMRChat",
+  emailVerification: "Xác minh email",
+  forgotPassword: "Quên mật khẩu",
+  signUp: "Đăng ký",
+};
