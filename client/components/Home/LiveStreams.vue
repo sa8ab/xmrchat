ce8d8a6721d7222f4bffd982f1d92624a67e9c5f
sa8ab <59901734+sa8ab@users.noreply.github.com>
2025-09-25 21:28:31 +0330

    Update checking fetch status

--- client/components/Home/LiveStreams.vue
+++ client/components/Home/LiveStreams.vue
@@ -19,7 +19,7 @@ const { data, status, error } = useLazyAsyncData(
   <div class="flex flex-col gap-4">
     <h3 class="text-center lg:text-start font-bold text-2xl">Live streams</h3>
     <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-4">
-      <template v-if="status === 'pending' && !data">
+      <template v-if="(status === 'pending' || status === 'idle') && !data">
         <USkeleton class="h-[180px] w-full" v-for="x in 3" />
       </template>
       <template v-else-if="data?.length">
