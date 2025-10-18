1f08f3be67dd065cc91d82614d3faa006ad73f33
sa8ab <59901734+sa8ab@users.noreply.github.com>
2025-10-18 21:05:10 +0330

    Add getting cohosts

--- client/pages/streamer/cohosts/index.vue
+++ client/pages/streamer/cohosts/index.vue
@@ -1,8 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { User } from "~/types";
+
+const { axios } = useApp();
+
+const { data } = useLazyAsyncData(
+  async () => {
+    const { data } = await axios.get<{ cohosts: User[] }>("/cohosts");
+    return data.cohosts;
+  },
+  { server: false }
+);
+</script>
 
 <template>
   <div>
     <PageTitle title="Cohosts" description="Manage your cohosts" />
+    <pre>{{ data }}</pre>
     <NoCohosts />
   </div>
 </template>
