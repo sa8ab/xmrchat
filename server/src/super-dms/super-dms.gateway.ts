9d97747b54a8b97b868e9af719a807288a4ac4bf
sa8ab <59901734+sa8ab@users.noreply.github.com>
2025-12-10 13:59:01 +0330

    Add sending streamer super dm message

--- server/src/super-dms/super-dms.gateway.ts
+++ server/src/super-dms/super-dms.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, UseGuards } from '@nestjs/common';
 import {
   MessageBody,
   OnGatewayConnection,
@@ -18,7 +18,9 @@ import { Repository } from 'typeorm';
 import { SuperDm } from './super-dm.entity';
 import * as openpgp from 'openpgp';
 import { SuperDmMessage } from './super-sm-message.entity';
-import { SuperDmMessageSenderType } from 'src/shared/constants';
+import { PageSettingKey, SuperDmMessageSenderType } from 'src/shared/constants';
+import { WsAuthGuard } from 'src/auth/guards/ws-auth.guard';
+import { PageSettingsService } from 'src/page-settings/page-settings.service';
 
 /**
  * @description Gateway for super DMs.
@@ -38,6 +40,7 @@ export class SuperDmsGateway
     @InjectRepository(SuperDm) private repo: Repository<SuperDm>,
     @InjectRepository(SuperDmMessage)
     private messagesRepo: Repository<SuperDmMessage>,
+    private pageSettingsService: PageSettingsService,
   ) {}
 
   @WebSocketServer()
@@ -71,6 +74,8 @@ export class SuperDmsGateway
     @MessageBody()
     body: SendMessageDto,
   ) {
+    // TODO: Validate body
+
     if (!body.superDmId) return { error: 'Super DM id is required' };
 
     const superDm = await this.repo.findOne({ where: { id: body.superDmId } });
@@ -109,6 +114,67 @@ export class SuperDmsGateway
       content: body.content,
       superDm: { id: superDm.id },
       senderType: SuperDmMessageSenderType.VIEWER,
+    });
+    await this.messagesRepo.save(created);
+
+    this.server
+      .to(`super-dm-${superDm.id}`)
+      .emit('super-dm-message', { superDmMessage: created });
+
+    return { message: 'message send', superDmMessage: created };
+  }
+
+  @SubscribeMessage('streamer-send-message')
+  @UseGuards(WsAuthGuard)
+  async streamerSendMessage(@MessageBody() body: SendMessageDto) {
+    if (!body.superDmId) return { error: 'Super DM id is required' };
+
+    const superDm = await this.repo.findOne({
+      where: { id: body.superDmId },
+      relations: { page: true },
+    });
+    if (!superDm) return { error: 'Super DM is not found' };
+
+    const pagePublicKeyArmored = await this.pageSettingsService.getSettingValue(
+      superDm.page.path,
+      PageSettingKey.SUPER_DM_PUBLIC_KEY,
+    );
+    if (!pagePublicKeyArmored)
+      return { error: 'Page public key is not configured' };
+
+    const pagePublicKey = await openpgp.readKey({
+      armoredKey: pagePublicKeyArmored,
+    });
+
+    const messageToVerify = JSON.stringify({
+      armoredMessage: body.content,
+      date: body.date,
+    });
+    const signature = await openpgp.readSignature({
+      armoredSignature: body.signature,
+    });
+
+    const verifyResult = await openpgp.verify({
+      message: await openpgp.createMessage({ text: messageToVerify }),
+      signature,
+      verificationKeys: [pagePublicKey],
+    });
+
+    try {
+      await verifyResult.signatures[0].verified;
+    } catch (error) {
+      return { error: 'Message is not verified' };
+    }
+
+    // time is not older than 1 minute
+    if (new Date(body.date) < new Date(Date.now() - 60 * 1000)) {
+      return { error: 'Message is too old' };
+    }
+
+    const created = this.messagesRepo.create({
+      content: body.content,
+      superDm: { id: superDm.id },
+      senderType: SuperDmMessageSenderType.CREATOR,
     });
     await this.messagesRepo.save(created);
 
