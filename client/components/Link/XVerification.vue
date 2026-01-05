7d540e9c6aabdf8fce94d1f3006cd7ad6e9dac82
sa8ab <59901734+sa8ab@users.noreply.github.com>
2026-01-05 21:25:58 +0330

    Add twitter verification

---
+++ client/components/Link/XVerification.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import useVuelidate from "@vuelidate/core";
+import { ContentLinkPlatformEnum } from "~/types/enums";
+
+const emit = defineEmits<{
+  verified: [];
+}>();
+
+const authStore = useAuthStore();
+const requestUrl = useRequestURL();
+const { axios } = useApp();
+const { required, url } = useValidations();
+const toast = useToast();
+
+const state = reactive({
+  url: "",
+  pending: false,
+});
+
+const verificationUrl = computed(() => {
+  const page = authStore.state.page;
+  if (!page) return "-";
+  return `${requestUrl.origin}/${page.path}`;
+});
+
+const handleVerify = async () => {
+  const valid = await v.value.$validate();
+  if (!valid) return;
+
+  state.pending = true;
+
+  try {
+    await axios.post("/link-verifications", {
+      type: ContentLinkPlatformEnum.X,
+      url: state.url.trim(),
+    });
+    toast.add({
+      color: "green",
+      title: "Verification successful!",
+    });
+    state.url = "";
+    emit("verified");
+  } catch (error) {
+    toast.add({
+      color: "red",
+      title: getErrorMessage(error),
+    });
+  } finally {
+    state.pending = false;
+  }
+};
+
+const v = useVuelidate(
+  {
+    url: { required, url },
+  },
+  computed(() => state)
+);
+
+const { getValidationAttrs } = useValidations(v);
+</script>
+
+<template>
+  <div>
+    <p>To auto-verify your Twitter account:</p>
+
+    <p class="pt-4">
+      1. Post a tweet containing your XMRChat link:
+      {{ verificationUrl }} (max 280 characters). Make sure to use this specific
+      verification link.
+    </p>
+    <p class="pt-2">
+      2. Then paste the full URL of the tweet in this form, for example:
+      https://x.com/MyUsername/status/123123123123123123 and click on Verify
+      Tweet.
+    </p>
+  </div>
+
+  <div class="pt-6">
+    <GeneralForm @submit="handleVerify">
+      <UFormGroup
+        label="Tweet URL"
+        name="url"
+        :error="getValidationAttrs('url').error"
+      >
+        <UInput
+          v-model="state.url"
+          placeholder="https://x.com/username/status/123123123123123123"
+          type="url"
+          @blur="getValidationAttrs('url').onBlur"
+        />
+      </UFormGroup>
+
+      <div class="flex mt-4">
+        <UButton type="submit" :loading="state.pending"> Verify Tweet </UButton>
+      </div>
+    </GeneralForm>
+  </div>
+</template>
+
+<style scoped></style>
--- client/pages/streamer/content-links/[type].vue
+++ client/pages/streamer/content-links/[type].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { ContentLink } from "~/types";
-import type { ContentLinkPlatformEnum } from "~/types/enums";
+import { ContentLinkPlatformEnum } from "~/types/enums";
 
 const { axios } = useApp();
 const route = useRoute();
@@ -22,9 +22,23 @@ const { data, pending, refresh, error } = await useLazyAsyncData(
   <PageTitle :title="`${getContentLink(type)?.name || '-'} Link`" />
 
   <div v-if="pending && !data"></div>
-  <ErrorView :error="error" v-else-if="error" />
+  <div v-else-if="error || !data || !data.value">
+    <UAlert color="red" variant="subtle">
+      <template #title> Link not found </template>
+      <template #description>
+        Link not found, please add and save the link first.
+      </template>
+    </UAlert>
+  </div>
   <div v-else>
-    <pre>{{ data }}</pre>
+    <template v-if="data.verification">
+      Verification
+      <pre>{{ data.verification }}</pre>
+    </template>
+    <template v-else>
+      <XVerification v-if="type === ContentLinkPlatformEnum.X" />
+      <div v-else>Verification is not supported for this link.</div>
+    </template>
   </div>
 </template>
 
