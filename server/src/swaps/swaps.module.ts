2a589a9fda76e9be945b36bfbf12b97ee1c620e8
sa8ab <59901734+sa8ab@users.noreply.github.com>
2026-01-08 17:44:26 +0330

    Min swap amount to 10 USD

--- server/src/swaps/swaps.module.ts
+++ server/src/swaps/swaps.module.ts
@@ -6,11 +6,13 @@ import { Coin } from 'src/integrations/trocador/coin.entity';
 import { TrocadorModule } from 'src/integrations/trocador/trocador.module';
 import { SwapsController } from './swaps.controller';
 import { NotificationsModule } from 'src/notifications/notifications.module';
+import { PricesModule } from 'src/prices/prices.module';
 
 @Module({
   imports: [
     TrocadorModule,
     NotificationsModule,
+    PricesModule,
     TypeOrmModule.forFeature([Swap, Coin]),
   ],
   providers: [SwapsService],
