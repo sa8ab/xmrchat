51cac770a5c368b8e93d9bd689e94a424aa6c59f
sa8ab <59901734+sa8ab@users.noreply.github.com>
2026-05-01 22:42:41 +0330

    Update paid content

--- client/pages/streamer/paid-content/[id]/edit.vue
+++ client/pages/streamer/paid-content/[id]/edit.vue
@@ -2,7 +2,11 @@
 
 <template>
   <div>
-    Page: streamer/offerings/[id]/edit
+    <PageTitle
+      title="Edit Paid Content"
+      description="Edit details of your paid content"
+    />
+    <PaidContentModifyForm />
   </div>
 </template>
 
--- client/pages/streamer/paid-content/[id]/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>
-    Page: streamer/offerings/[id]/index
-  </div>
-</template>
-
-<style scoped></style>
