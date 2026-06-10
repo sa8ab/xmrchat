5e7ac005dbd21d80d70c94aef3855105eebc2840
sa8ab <59901734+sa8ab@users.noreply.github.com>
2026-06-11 01:08:06 +0330

    Add bio on page

--- client/types/general.ts
+++ client/types/general.ts
@@ -50,6 +50,7 @@ export interface CreateFormFields {
   messageTipDisplayMode?: TipDisplayMode;
   fiat?: FiatEnum;
   expirationMinutes?: number;
+  bio?: string;
 }
 
 export interface TipFormFields {
