999ef1dc97e22ce56ab10bab5f08407f0f90e67c
sa8ab <59901734+sa8ab@users.noreply.github.com>
2026-06-25 19:16:04 +0330

    Reply colors for premium

--- server/src/tip-replies/tip-reply-settings.service.ts
+++ server/src/tip-replies/tip-reply-settings.service.ts
@@ -1,15 +1,26 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PagesService } from 'src/pages/pages.service';
 import { PageSettingsService } from 'src/page-settings/page-settings.service';
-import { PageSettingCategory, PageSettingKey } from 'src/shared/constants';
+import {
+  Action,
+  PageSettingCategory,
+  PageSettingKey,
+} from 'src/shared/constants';
 import { User } from 'src/users/user.entity';
 import { UpdateTipReplySettingsDto } from './dtos/update-tip-reply-settings.dto';
+import { TipReply } from './tip-reply.entity';
+import { CaslAbilityFactory } from 'src/casl/casl-ability.factory';
 
 @Injectable()
 export class TipReplySettingsService {
   constructor(
     private pagesService: PagesService,
     private pageSettingsService: PageSettingsService,
+    private caslAbilityFactory: CaslAbilityFactory,
   ) {}
 
   async getSettings(user: User) {
@@ -29,6 +40,12 @@ export class TipReplySettingsService {
   async updateSettings(dto: UpdateTipReplySettingsDto, user: User) {
     const page = await this.pagesService.findMyPage(user);
     if (!page) throw new NotFoundException('Page not found');
+
+    const ability = await this.caslAbilityFactory.createForUser(user);
+    if (!ability.can(Action.Update, TipReply))
+      throw new UnauthorizedException(
+        'You are not authorized to update tip reply settings.',
+      );
 
     await this.pageSettingsService.upsert(
       page.id,
